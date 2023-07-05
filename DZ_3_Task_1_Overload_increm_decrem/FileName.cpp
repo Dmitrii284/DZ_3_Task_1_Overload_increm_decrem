cab234214458699052d1906f1091bd4fb20fbaba
Дмитрий <@ADMIN>
2023-07-05 15:51:29 +0500

    2

--- DZ_3_Task_1_Overload_increm_decrem/FileName.cpp
+++ DZ_3_Task_1_Overload_increm_decrem/FileName.cpp
@@ -19,11 +19,11 @@ public:
 	}
 
 	// Умножение точки (1,1) На число value
-	//friend const Fraction operator+(const Fraction& point, double value)
-	//{
-	//	return Fraction(point._x * value, point._y * value);
-	//	//return point + value;
-	//}
+	friend const Fraction operator+(const Fraction& point, double value)
+	{
+		return Fraction(point._x * value, point._y * value);
+		//return point + value;
+	}
 
 	// Унарный минус Перегрузка оператора (-) изменение знака точки На противоположный
 	const Fraction operator-()
@@ -44,11 +44,11 @@ public:
 	}
 
 	// Перегрузка оператора (*) Координаты точки (1,1) на число  value
-	//friend const Fraction operator*(const Fraction& point, double value)
-	//{
-	//	//return Fraction(point._x * value, point._y * value);
-	//	return point * value;
-	//}
+	friend const Fraction operator*(const Fraction& point, double value)
+	{
+		//return Fraction(point._x * value, point._y * value);
+		return point * value;
+	}
 
 	// Перегрузка оператора (/) Деление двух точек
 	friend const Fraction operator/(const Fraction & point1, const Fraction & point2)
@@ -57,11 +57,11 @@ public:
 	}
 
 	// Деление координаты точки (1,1) на число value
-	//friend const Fraction operator/(const Fraction& point, double value)
-	//{
-	//	//return Fraction(point._x / value, point._y / value);
-	//	return point / value;
-	//}
+	friend const Fraction operator/(const Fraction& point, double value)
+	{
+		//return Fraction(point._x / value, point._y / value);
+		return point / value;
+	}
 
 };
 
@@ -69,16 +69,28 @@ int main()
 {
 	Fraction A(2.2, 4.5);
 	Fraction B(4.8, 5.5);
-	Fraction C = A - B;
-	Fraction D = (A * B);
-	Fraction E = (A / B);
-	Fraction K = (A + B);
+	double a = 10;
+
+
+	Fraction C1 = (A,a);
+	C1.Show();
+
+	/*Fraction C = A - B;
 	C.Show();
-	C.operator-();
-	C.Show();	
+
+	Fraction D = (A * B);
 	D.Show();
+
+	Fraction E = (A / B);
 	E.Show();
+
+	Fraction K = (A + B);
 	K.Show();
+
+	C.operator-();
+	C.Show();	*/
+	
+	
 
 	/*A.operator-();
 	A.Show();
